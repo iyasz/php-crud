3c92ec64efe0c291963f53af76536c0caa54ecbe
Yasz Avellia <zakamaragames@gmail.com>
2022-08-17 07:43:29 +0700

    bootstrap link cdn

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php  ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +8,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crud Test</title>
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h1>Input Data</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
