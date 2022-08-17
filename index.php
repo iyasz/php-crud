db9931c7190ece56fb9466f445651bcebf196869
Yasz Avellia <zakamaragames@gmail.com>
2022-08-17 08:02:13 +0700

    input tahap 2

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,31 +11,47 @@
     <title>Crud Test</title>
     <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
 </head>
+
 <body>
     <div class="container mt-5">
         <div class="row justify-content-center">
-            <div class="col-md-12">
+            <div class="col-lg-12">
                 <div class="card shadow-lg">
-                    <div class="card-header text-bg-dark">
+                    <div class="card-header text-bg-dark ps-4">
                         <h1>Input Data</h1>
                     </div>
-                    <div class="card-body">
-                        <form action="" method="post">
+                    <form action="" method="post">
+                        <div class="card-body">
+                            <div class="col-md-6">
+                                <div class="form-group my-3">
+                                    <label for="username">Username</label>
+                                    <input type="text" id="username" placeholder="Masukan Username " class="form-control">
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="kontak">Kontak</label>
+                                    <input type="text" id="kontak" placeholder="Masukan Nama Kontak" class="form-control">
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="telp">No. Telp</label>
+                                    <input type="number" id="telp" placeholder="Masukan No. Telp " class="form-control">
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="alamat">Alamat</label>
+                                    <input type="text" id="alamat" placeholder="Masukan Alamat " class="form-control">
+                                </div>
+                            </div>
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label for="username">Username</label>
-                                    <input type="text" id="username" placeholder="Masukan Username Anda" class="form-control" >
+                                <label for="">asw</label>
                                 </div>
-                                <div class="form-group"></div>
-                                <div class="form-group"></div>
-                                <div class="form-group"></div>
                             </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
     <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
