446989e16128f32338fdaa0e4d47079c86f0c62c
Yasz Avellia <zakamaragames@gmail.com>
2022-08-17 07:47:51 +0700

    input form tahap 1

--- index.php
+++ index.php
@@ -11,12 +11,25 @@
     <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <div class="container">
-        <div class="row">
+    <div class="container mt-5">
+        <div class="row justify-content-center">
             <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
+                <div class="card shadow-lg">
+                    <div class="card-header text-bg-dark">
                         <h1>Input Data</h1>
+                    </div>
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="username">Username</label>
+                                    <input type="text" id="username" placeholder="Masukan Username Anda" class="form-control" >
+                                </div>
+                                <div class="form-group"></div>
+                                <div class="form-group"></div>
+                                <div class="form-group"></div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
